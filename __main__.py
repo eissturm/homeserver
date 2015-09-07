3e23518f05e95619f333bc2d58e667be849cfb0f
Andrew Meyer <andymeyer747@gmail.com>
2015-09-06 23:38:56 -0500

    Thanks CrashPlan!

--- __main__.py
+++ __main__.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
-from flask import Flask
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/butt')
+def butt():
+    return 'Butts!'
+
+@app.route('/admin/')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('login.html')
+    
 
 
 if __name__ == '__main__':
-    main()
+    app.debug = True
     app.run(host='0.0.0.0',port=80)
