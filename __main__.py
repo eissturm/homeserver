07e4e88b192de633271cd90a135b2d3762089128
Andrew Meyer <andymeyer747@gmail.com>
2015-09-30 22:06:16 -0500

    my cat is a egyptian demi-god of war

--- __main__.py
+++ __main__.py
@@ -24,8 +24,16 @@ def login():
     else:
         return render_template('login.html')
     
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+    
 
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run(host='0.0.0.0',port=80)
